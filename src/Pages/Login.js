24067f8a1d416a398b3a9ef9a04647d2c37c59d4
kainthekiller <phillipyates12@hotmail.com>
2022-10-31 15:45:47 -0500

    Login Page Started

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,7 +6,10 @@ import {Button} from "@mui/material";
 function Login()
 {
     return (
-        <>
+        <div className={"LoginPageInput"}>
+            <header>
+                <img className={"LoginImg"} src={"https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} alt={""}/>
+            </header>
         <input
         type={"text"}
         placeholder={"UserName"}
@@ -16,7 +19,8 @@ function Login()
                 placeholder={"Password"}
             />
             <Button variant={"contained"}>Login</Button>
-        </>
+
+        </div>
     )
 }
 
