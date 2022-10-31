d9e8a3f6bbf50eb7a6cf257916079a7f6ce0bc5e
kainthekiller <phillipyates12@hotmail.com>
2022-10-31 13:51:57 -0500

    Continue Front End Design Work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from "@mui/material";
 
 function App() {
   return (
       <>
     <div className="MainPageHead">
       <h1>Welcome To PMCS</h1>
+      <div className="NavLinks">
+
+      <Link sx={{m: 2, backgroundColor: "Black", color: "Yellow", padding: 2, paddingLeft: 4, paddingRight: 4}} href="#" underline="hover">
+        {'Main'}
+      </Link>
+
+
+      <Link sx={{m: 2, backgroundColor: "Black", color: "Yellow", padding: 2}} href="#" underline="hover">
+        {'See All TM'}
+      </Link>
+
+      <Link sx={{m: 2, backgroundColor: "Black", color: "Yellow", padding: 2, paddingLeft: 3, paddingRight: 3}} href="#" underline="hover">
+        {'Find TM'}
+      </Link>
+
+      </div>
     </div>
       </>
   );
